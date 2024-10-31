612dcb113872e0d6c333df56a934c5851d8d62ff
Renatuscape <silarcta@gmail.com>
2024-10-31 14:38:34 +0100

    Implements the functionality of repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (input, repeatCount) {
+    if (repeatCount < 0) {
+        return 'ERROR';
+    }
+    return input.repeat(repeatCount);
 };
 
 // Do not edit below this line
