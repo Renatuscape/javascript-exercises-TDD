e296261357cb9d7f66997b78ccb361e71fb6d16b
Renatuscape <silarcta@gmail.com>
2024-11-01 13:27:23 +0100

    Completes fibonacci tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (fibNumber) {
+    if (fibNumber < 0) {
+        return 'OOPS';
+    }
 
+    const fibSequence = [0, 1];
+
+    while (fibSequence.length <= fibNumber){
+        fibSequence.push(fibSequence[fibSequence.length - 2] + fibSequence[fibSequence.length - 1])
+    }
+
+    console.log(fibNumber + " returned " + fibSequence[fibNumber] + ". Length of fibSequence is " + fibSequence.length);
+    return fibSequence[fibNumber];
 };
 
 // Do not edit below this line
