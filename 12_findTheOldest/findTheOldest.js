406fba82a59560a29cd3cde7c83cd0a9766b1853
Renatuscape <silarcta@gmail.com>
2024-11-01 13:58:54 +0100

    Completes findTheOldest tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,5 @@
-const findTheOldest = function() {
-
+const findTheOldest = (people) => {
+    return people.sort((b, a) => ((a.yearOfDeath ?? new Date().getFullYear()) - a.yearOfBirth) - ((b.yearOfDeath ?? new Date().getFullYear()) - b.yearOfBirth))[0];
 };
 
 // Do not edit below this line
