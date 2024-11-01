a3bf908f7a259febd542a53b62587adf464973e7
Renatuscape <silarcta@gmail.com>
2024-11-01 09:00:06 +0100

    Completes removeFromArray tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...restArgs) {
+    for (i = 0; i < restArgs.length; i++) {
+        let index = array.indexOf(restArgs[i]);
+
+        while (index > -1) {
+            array.splice(index, 1);
+            index = array.indexOf(restArgs[i]);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
