c057e34e8715ba4b2381c2cd9dcd03360b172c07
Renatuscape <silarcta@gmail.com>
2024-11-01 12:19:55 +0100

    Completes calculator tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  array.forEach((number) => { sum += number });
+  
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (array) {
+  let sum = array[0];
 
+  for (i = 1; i < array.length; i++) {
+    sum = sum * array[i];
+  }
+
+  return sum;
 };
 
-const power = function() {
-	
+const power = function (num, power) {
+  return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) {
+    return 1;
+  }
+
+  let sum = 1;
+  for (i = 2; i <= num; i++) {
+    sum *= i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
