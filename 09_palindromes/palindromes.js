5df88a3909a517f1852c3f9981383e00210329c5
Renatuscape <silarcta@gmail.com>
2024-11-01 13:08:27 +0100

    Completes palindromes tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (stringInput) {
+    stringInput = stringInput.toLowerCase().replace(/\W/g, '');
 
+    const reversedString = stringInput.split('').reverse().join('');
+    return reversedString === stringInput;
 };
 
 // Do not edit below this line
