4af10439e1bbcabc9a4fb04cf4cb665939ae4dd1
Renatuscape <silarcta@gmail.com>
2024-11-01 10:14:05 +0100

    Completes sumAll tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function (startNumber, endNumber) {
+    if (!Number.isInteger(startNumber) || !Number.isInteger(endNumber) || startNumber < 0 || endNumber < 0) {
+        return 'ERROR';
+    }
 
+    let sum = 0;
+    for (i = Math.min(startNumber, endNumber); i <= Math.max(startNumber, endNumber); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
