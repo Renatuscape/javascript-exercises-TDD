8dbc485eb79b5a17527da59903005b89609523b3
Renatuscape <silarcta@gmail.com>
2024-11-01 10:39:07 +0100

    Completes tempConversion tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheitIn) {
+  return Math.round(((farenheitIn - 32) / 1.8) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celciusIn) {
+  return Math.round(((celciusIn * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
